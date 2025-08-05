a1d422e30dbf55e4673667d875b08325da1c186d
Isam6 <issamdiez10@gmail.com>
2025-08-05 14:17:48 +0200

    Adding Xcode default files

---
+++ LumiCloud/LumiCloud/LumiCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumiCloudApp.swift
+//  LumiCloud
+//
+//  Created by Isam El Mourabet on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LumiCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
