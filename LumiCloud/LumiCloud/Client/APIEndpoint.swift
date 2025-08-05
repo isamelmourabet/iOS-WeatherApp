81aa04fe0e4bd9eba036d2752fe98ef0d78f1c09
Isam6 <issamdiez10@gmail.com>
2025-08-05 16:54:56 +0200

    Adding endpoints

---
+++ LumiCloud/LumiCloud/Client/APIEndpoint.swift
@@ -0,0 +1,31 @@
+//
+//  APIEndpoint.swift
+//  LumiCloud
+//
+//  Created by Isam El Mourabet on 5/8/25.
+//
+
+import Foundation
+
+enum APIEndpoint {
+    
+    static let baseURLString = "https://api.openweathermap.org"
+    
+    case coordinatesByLocationName(String)
+    case weatherByLocation(Double, Double)
+    
+    private var path: String {
+        switch self {
+        case .coordinatesByLocationName(let city):
+            return "/geo/1.0/direct?q=\(city)&appid=\(Constants.Keys.weatherAPIKey)"
+        case .weatherByLocation(let lat, let lon):
+            return "/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(Constants.Keys.weatherAPIKey)"
+        }
+    }
+    
+    static func endpointURL(for endpoint: APIEndpoint) -> URL {
+        let endpointPath = endpoint.path
+        return URL(string: "\(baseURLString)\(endpointPath)")!
+    }
+    
+}
---
+++ LumiCloud/LumiCloud/Utilities/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  LumiCloud
+//
+//  Created by Isam El Mourabet on 5/8/25.
+//
+
+import Foundation
+
+struct Constants {
+
+    struct Keys {
+        static let weatherAPIKey = "f60d3062e982a5927fa1dc88f6813ba6"
+    }
+    
+}
