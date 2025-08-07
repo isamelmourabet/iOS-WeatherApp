0d220a74e4271ba6c23de09ac31244774f0b2814
Isam6 <issamdiez10@gmail.com>
2025-08-07 16:04:16 +0200

    Updating app view in ContentView.swift

--- LumiCloud/LumiCloud/ContentView.swift
+++ LumiCloud/LumiCloud/ContentView.swift
@@ -8,26 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var city: String = ""
+    @State private var isFectchingWeather: Bool = false
+
+    let geocodingClient = GeocodingClient()
+    let weatherClient = WeatherClient()
+    
+    @State private var weather: Weather?
+    
+    private func fetchWeather() async {
+        do {
+            guard let location = try await geocodingClient.coordinateByCity(city) else { return }
+            weather = try await weatherClient.fetchWeather(location: location)
+        } catch {
+            print(error)
+        }
+        
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            
-            Button("Ver coordenadas") {
-                Task {
-                    let geocodingClient = GeocodingClient()
-                    let weatherClient = WeatherClient()
+        NavigationStack {
+            ZStack {
+                
+                Color.cyan.opacity(0.5)
+                    .ignoresSafeArea(.all)
+                
+                VStack {
+                    TextField("Introduce una ciudad", text: $city)
+                        .textFieldStyle(.roundedBorder)
+                        .onSubmit {
+                            isFectchingWeather = true
+                        } .task(id: isFectchingWeather) {
+                            if isFectchingWeather {
+                                await fetchWeather()
+                                isFectchingWeather = false
+                                city = ""
+                            }
+                        }
+                    Spacer()
                     
-                    let location = try! await geocodingClient.coordinateByCity("Alicante")
-                    let weather = try! await weatherClient.fetchWeather(location: location!)
+                    if let weather {
+                        Text(MeasurementFormatter.temperature(value: weather.temp))
+                            .font(.system(size: 64))
+                            .padding()
+                        
+                        HStack {
+                            VStack {
+                                Text("Min")
+                                Text(MeasurementFormatter.temperature(value: weather.temp_min))
+                            }
+                            .font(.title2)
+                            .padding(10)
+                            .glassEffect(.regular .interactive() .tint(.blue))
+                            
+                            VStack {
+                                Text("Max")
+                                Text(MeasurementFormatter.temperature(value: weather.temp_max))
+                            }
+                            .font(.title2)
+                            .padding(10)
+                            .glassEffect(.regular .interactive() .tint(.red))
+                        }
+                        
+                        
+                        Text("Sensación térmica: " + MeasurementFormatter.temperature(value: weather.feels_like))
+                            .padding()
+                    }
                     
-                    print(weather)
+                    Spacer()
                 }
+                .padding()
+                .navigationTitle("Temperatura")
             }
         }
-        .padding()
     }
 }
 
