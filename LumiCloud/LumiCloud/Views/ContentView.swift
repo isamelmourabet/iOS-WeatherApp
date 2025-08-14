34086bf5c4627220a903ebeae2c4f68433b23e77
Isam6 <issamdiez10@gmail.com>
2025-08-14 22:56:35 +0200

    Update ContentView.swift

---
+++ LumiCloud/LumiCloud/Views/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  LumiCloud
+//
+//  Created by Isam El Mourabet on 5/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var city: String = ""
+    @State private var city2: String = ""
+    @State private var isFectchingWeather: Bool = false
+
+    let geocodingClient = GeocodingClient()
+    let weatherClient = WeatherClient()
+    let weatherInformationClient = WeatherInformationClient()
+    
+    @State private var weather: Weather?
+    @State private var information: [Information?] = []
+    @State private var location: Location?
+    @State private var weatherList: [Location] = []
+    
+    private func fetchWeather() async {
+        do {
+            //guard let location = try await geocodingClient.coordinateByCity(city) else { return }
+            location = try await geocodingClient.coordinateByCity(city)
+            //cityName = location.name
+            weather = try await weatherClient.fetchWeather(location: location!)
+            weatherList.append(location!)
+            
+            information = try await weatherInformationClient.fetchWeatherInformation(location: location!)
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                
+                Color.cyan.opacity(0.5)
+                    .ignoresSafeArea(.all)
+                
+                VStack {
+                    TextField("Introduce una ciudad", text: $city)
+                        .textFieldStyle(.roundedBorder)
+                        .onSubmit {
+                            isFectchingWeather = true
+                        } .task(id: isFectchingWeather) {
+                            if isFectchingWeather {
+                                await fetchWeather()
+                                isFectchingWeather = false
+                                city = ""
+                            }
+                        }
+                    
+                    Spacer()
+                    
+                    if let info = information.first {
+                        Text(info!.description.localizedWeatherDescription(for: Locale(identifier: "es")))
+                            .padding()
+                    }
+                                        
+                    if let weather {
+                        WeatherCardView(weather: weather, location: location)
+                    }
+                    
+                    Spacer()
+                }
+                .padding()
+                .navigationTitle("Temperatura")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
